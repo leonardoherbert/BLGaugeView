3995836f0a6fcde29dd915e08d674552b52e60cc
Bruno Lima <brunolimasud@gmail.com>
2017-09-25 10:14:30 -0300

    fixing bg radius

--- BLGaugeView/BLGaugeView.swift
+++ BLGaugeView/BLGaugeView.swift
@@ -49,7 +49,7 @@ open class BLGaugeView: UIView {
     var bgRadius: CGFloat {
         get {
             let centerValue = centerY < centerX ? centerY : centerX
-            return centerValue - (centerValue*0.25)
+            return centerValue - (centerValue*0.3)
         }
     }
     
